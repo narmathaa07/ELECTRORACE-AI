4a8f5d8f277b8d45fc5b961e734b8494e546feda
narmathaa07 <24004481@siswa.um.edu.my>
2026-05-02 14:49:58 +0800

    Delete core/prediction.py

--- core/prediction.py
+++
@@ -1,6 +0,0 @@
-def predict_bill(df):
-    avg_power = df["power"].mean()
-    tariff = 0.218  # RM per kWh approx
-    daily_kwh = avg_power / 1000 * 24
-    monthly_bill = daily_kwh * 30 * tariff
-    return round(monthly_bill, 2)
