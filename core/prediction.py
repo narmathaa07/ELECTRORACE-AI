f5dfdf63141eb6c46b8b0c8e8191fc81cb3a7833
narmathaa07 <24004481@siswa.um.edu.my>
2026-05-02 14:32:00 +0800

    Refactor predict_bill to calculate monthly bill from DataFrame

--- core/prediction.py
+++ core/prediction.py
@@ -1,3 +1,6 @@
-def predict_bill(total_kwh):
-
-    return round(total_kwh * 0.6, 2)
+def predict_bill(df):
+    avg_power = df["power"].mean()
+    tariff = 0.218  # RM per kWh approx
+    daily_kwh = avg_power / 1000 * 24
+    monthly_bill = daily_kwh * 30 * tariff
+    return round(monthly_bill, 2)
